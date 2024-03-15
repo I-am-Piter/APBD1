3134fa9fbac1fd86bb072a83bffdbc182bc0ceba
Piotr Sułek <piotrsulek123@gmail.com>
2024-03-15 09:12:26 +0100

    max

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,8 +8,14 @@ Console.WriteLine("Hello, World!");
 int[] integers = {1, 2, 3, 4, 5};
 
 Console.WriteLine(CalculateSmth(integers));
+Console.WriteLine(CalculateMax(integers));
 
 static double CalculateSmth(int[] tab)
 {
     return tab.Average();
+}
+
+static double CalculateMax(int[] tab)
+{
+    return tab.Max();
 }
